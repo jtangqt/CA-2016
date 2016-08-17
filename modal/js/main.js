d239dd7f54079f6593597a0be3b639cd200890ff
jtangqt <ajasmineflower@gmail.com>
2016-08-16 20:29:45 -0400

    added some things to modal homework

--- modal/js/main.js
+++ modal/js/main.js
@@ -1,20 +1,18 @@
-var modal = document.getElementById("modal");
-var button = document.getElementById("click");
-var x = document.getElementsByClassName("close")[0];
+$(document).on("ready",function(){
+	$("button").on("click",function(){
+		$("body").addClass("overlay");
+	});
+	
+	$("#lightbox button").on("click",function(){
+		$("body").removeClass("overlay");
+	});
+
+//if the window has the overlay
+	// if(){
+	// 	$(window).on("click", function(){
+	// 		$("body").removeClass("overlay");
+	// 	});
+	// }
 
 
-
-button.onclick = function(){
-	modal.style.display= "block";
-	console.log("hello");
-};
-
-x.onclick = function(){
-	modal.style.display = "none";
-};
-
-window.onclick= function(event){
-	if (event.target == modal){
-		modal.style.display = "none";
-	}
-};
+});
